8ba8b79dea74d30edb2035954bb219d29c389934
MestDoranbolt <noob12321@gmail.com>
2018-01-20 16:54:42 +0100

    Working mapping

--- src/com/mest/hibernate/app/CreatePracownik.java
+++ src/com/mest/hibernate/app/CreatePracownik.java
@@ -4,7 +4,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-import com.mest.hibernate.app.entity.Zmiany;
+import com.mest.hibernate.app.entity.Pracownik;
 
 public class CreatePracownik {
 
@@ -12,16 +12,21 @@ public class CreatePracownik {
 		// Create Session Factory
 		
 		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
-		
-		Session session = factory.getCurrentSession();
-		
-		try {
-
-		}
-		finally {
-			factory.close();
-		}
-
-	}
-
+	       
+        Session session = factory.getCurrentSession();
+        session.beginTransaction();
+       
+        Pracownik pracownik = new Pracownik();
+        pracownik.setImie("Karolina");
+        pracownik.setNazwisko("Sêtorek");
+        pracownik.setDataZatrudnienia(java.sql.Date.valueOf("2017-09-25"));
+        pracownik.setStanowisko("Kierownik");
+        pracownik.setPensja(3000);
+        pracownik.setKwalifikacje("blablabla");
+        session.save(pracownik);
+        session.getTransaction().commit();
+        factory.close();
+       
+            System.out.println(pracownik.getImie() + "\t" + pracownik.getNazwisko() + "\t" + pracownik.getStanowisko());
+}
 }
